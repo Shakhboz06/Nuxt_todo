ec5dfb3f8ad2b124e88c8e2a8e5148fa9344bbae
Shakhboz06 <shakhboz.shodiev2001@gmail.com>
2022-02-20 22:35:22 +0500

    my stack

--- pages/Dashboard.vue
+++ pages/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dashboard" :class="{bg_active: bgActive}">
+  <div class="dashboard" :class="{ bg_active: bgActive }">
     <div class="wrapper">
       <div class="headers">
         <div class="heading">
@@ -8,11 +8,19 @@
         </div>
         <div class="subheading">
           <span>Показать задачи как:</span>
-          <p class="table" @click="Change_table()" :class="{'tabWhite': isBlue}">Таблица</p>
-          <p class="tile" @click="Change_view()"  :class="{'tabBlue': isBlue}">Плитка</p>
+          <p
+            class="table"
+            @click="Change_table()"
+            :class="{ tabWhite: isBlue }"
+          >
+            Таблица
+          </p>
+          <p class="tile" @click="Change_view()" :class="{ tabBlue: isBlue }">
+            Плитка
+          </p>
         </div>
       </div>
-      <div class="table_bar" :class="{'bar_active': tileActive}">
+      <div class="table_bar" :class="{ bar_active: tileActive }">
         <div class="title">
           <div class="title_block_1">
             <span class="heading">Заголовок задачи</span>
@@ -25,7 +33,12 @@
           </div>
         </div>
         <div class="taskbar">
-          <div class="item" v-for="item of this.data" :key="item.key" :class="{'ItemActive': isItem}">
+          <div
+            class="item"
+            v-for="item of this.data"
+            :key="item.key"
+            :class="{ ItemActive: isItem }"
+          >
             <div class="task_block_1">
               <p class="heading">{{ item.title }}</p>
               <p class="info">
@@ -35,21 +48,33 @@
             <div class="task_block_2">
               <p class="date">{{ item.date }}</p>
               <p class="time">{{ item.time }}</p>
-              <p class="Check_Up" :class="{isRed: item.check == 'Не выполнено', isBlue: item.check == 'В прогрессе' }">
+              <p
+                class="Check_Up"
+                :class="{
+                  isRed: item.check == 'Не выполнено',
+                  isBlue: item.check == 'В прогрессе',
+                }"
+              >
                 {{ item.check }}
               </p>
             </div>
-             <p class="tile_Up" :class="{isRed: item.check == 'Не выполнено', isBlue: item.check == 'В прогрессе' }">
-                {{ item.check }}
-              </p>
+            <p
+              class="tile_Up"
+              :class="{
+                isRed: item.check == 'Не выполнено',
+                isBlue: item.check == 'В прогрессе',
+              }"
+            >
+              {{ item.check }}
+            </p>
           </div>
         </div>
       </div>
       <div class="modal modal-edit" :class="{ active: isActive }">
         <form @submit.prevent="ADD_TASK()">
           <h2>Добавить</h2>
-          <img src="~/static/2561211_circle_x_icon.svg" @click="HideBar()" />
-          <input type="text" name="title" placeholder="Заголовок" />
+          <img src="~/static/2561211_circle_x_icon.svg" @click="HideBar()"/>
+            <input type="text" name="title" placeholder="Заголовок" />
           <input type="text" name="description" placeholder="Описание" />
           <input type="date" name="date" placeholder="Дата" />
           <input type="time" name="time" placeholder="Время" />
@@ -77,7 +102,7 @@ export default {
       bgActive: false,
       tileActive: false,
       isBlue: false,
-      isItem: false
+      isItem: false,
     };
   },
   computed: {
@@ -85,27 +110,25 @@ export default {
   },
   methods: {
     HideBar: function () {
-      this.isActive = false,
-      this.bgActive = false
-      this.isItem = false
+      (this.isActive = false), (this.bgActive = false);
+      this.isItem = false;
     },
     AddTask: function () {
-      this.isActive = true,
-      this.bgActive = true
-      this.isItem = true
+      (this.isActive = true), (this.bgActive = true);
+      this.isItem = true;
     },
-    Change_view: function(){
-      this.tileActive = true
-      this.isBlue = true
+    Change_view: function () {
+      this.tileActive = true;
+      this.isBlue = true;
     },
-    Change_table: function(){
-      this.tileActive = false
-      this.isBlue = false
+    Change_table: function () {
+      this.tileActive = false;
+      this.isBlue = false;
     },
     ...mapActions(["ADD_TASK"]),
   },
 };
 </script>
 
-<style>
+</style>
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="home">
+
   </div>
 </template>
 
